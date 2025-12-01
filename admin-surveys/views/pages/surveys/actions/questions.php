0e7ff171d0832997cc06c755bd489eaad1f09a4a
Osvaldo1964 <140724795+Osvaldo1964@users.noreply.github.com>
2025-12-01 15:32:10 -0500

    adjust_questins_001

--- admin-surveys/views/pages/surveys/actions/questions.php
+++ admin-surveys/views/pages/surveys/actions/questions.php
@@ -69,9 +69,9 @@ if (isset($routesArray[3])) {
             </div>
             <!-- Div inferior derecho para crear o editar preguntas tipo Opcion  -->
             <div class="col-md-6 notblock divOptions border mt-1" style="float: right; text-align: center; height: 310px;" id="divOptions">
-                <div class="col-md-6 border mt-1 mx-auto d-flex flex-column" style="float: left; height: 300px;">
+                <div class="col-md-6 border mt-1 mx-auto d-flex flex-column" style="float: left; height: 300px; overflow: auto;">
                     <div class="table responsive" style="height: 300px;" id="tableOptions">
-                        <div class="table-responsive" style="overflow: auto;">
+                        <div class="table-responsive">
                             <table class="table table-bordered table-striped mt-1" id="tableOptions">
                                 <thead style="text-align: center; font-size: 12px;">
                                     <tr>
